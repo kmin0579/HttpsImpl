73c0bf6020163650e3447965550c89b4270f15d5
kmin <534470831@qq.com>
2022-12-17 23:01:06 +0800

    fix CMake version finish ServerHello

--- src/base_struct.h
+++ src/base_struct.h
@@ -6,17 +6,29 @@
 typedef uint16_t ProtocolVersion;
 
 template<typename LENGTH_TYPE, uint64_t MAX_LENGTH>
-struct VariantVector {
+class VariantVector {
+public:
 	LENGTH_TYPE length;
 	unsigned char data[MAX_LENGTH];
 };
 
-struct ClientHello {
+class ClientHello {
+private:
 	ProtocolVersion legacy_version; // 0x0303: TLS v1.2
 	unsigned char random[32];
 	VariantVector<uint8_t, 32> legacy_session_id;
 	VariantVector<uint16_t, 1 << 16> cipher_suites;
 	VariantVector<uint8_t, 1 << 8> legacy_compression_methods;
+	VariantVector<uint16_t, 1 << 16> extensions;
+};
+
+class ServerHello {
+private:
+	ProtocolVersion legacy_version; // 0x0303: TLS v1.2
+	unsigned char random[32];
+	VariantVector<uint8_t, 32> legacy_session_id_echo;
+	uint8_t cipher_suite[2];
+	uint8_t legacy_compression_method;
 	VariantVector<uint16_t, 1 << 16> extensions;
 };
 
