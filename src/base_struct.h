a4e7d63118f060203c19f58b00f1c132562b6bce
kmin <534470831@qq.com>
2022-12-17 22:48:33 +0800

    complete ClientHello

--- src/base_struct.h
+++ src/base_struct.h
@@ -11,15 +11,13 @@ struct VariantVector {
 	unsigned char data[MAX_LENGTH];
 };
 
-struct CipherSuite {
-	uint8_t data[2];
-};
-
 struct ClientHello {
 	ProtocolVersion legacy_version; // 0x0303: TLS v1.2
 	unsigned char random[32];
 	VariantVector<uint8_t, 32> legacy_session_id;
-	VariantVector<uint16_t, 1 << 16> legacy_compression_methods;
+	VariantVector<uint16_t, 1 << 16> cipher_suites;
+	VariantVector<uint8_t, 1 << 8> legacy_compression_methods;
+	VariantVector<uint16_t, 1 << 16> extensions;
 };
 
 #endif
